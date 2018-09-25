1ba8b05d1b061672d07560216252ee45f646057b
Андрей Арефьев <andy@arefyev.su>
2018-09-25 07:15:21 +0500

    add a sandbox project

---
+++ WebSandBox/WebSandBox/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebSandBox.MvcApplication" Language="C#" %>
---
+++ WebSandBox/WebSandBox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSandBox.Startup))]
+namespace WebSandBox
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
